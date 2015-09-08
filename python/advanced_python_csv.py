ed1f245537fee382a44fa2c628d126c40054db61
David Berger <DBERGER1989@GMAIL.COM>
2015-09-08 12:52:35 -0400

    added code

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,22 @@
-PLACE YOUR CODE HERE
+import csv
+fpath = '/Users/David/ds/metis/prework/dsp/python/faculty.csv'
+import re
+
+fin = open(fpath, 'rb')
+dfile = csv.DictReader(fin)
+
+
+emlist = []
+for item in dfile:
+    emlist.append(item[' email'])
+
+emsublists = [[item] for item in emlist]
+
+
+tfile = open('email_list.csv', 'wb') 
+
+csv_writer = csv.writer(tfile)
+
+csv_writer.writerows(emsublists)
+
+tfile.close()
