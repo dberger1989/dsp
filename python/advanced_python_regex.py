42d633689064843652a76fee237473acafd8b118
David Berger <DBERGER1989@GMAIL.COM>
2015-09-07 23:12:38 -0400

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,1 +1,25 @@
-PLACE YOUR CODE HERE
+#Part 1, Question 1:
+#to count how many types of degrees there are:
+import csv
+import re
+fpath = '/Users/David/Desktop/faculty.csv'
+
+fin = open(fpath, 'rb')
+firead = csv.reader(fin)
+print x[1].replace('.', '') for x[1] in firead
+
+degrees = []
+for item in firead:
+    subdot = item[1].replace('.', '')
+    degrees.append(subdot)
+
+degsp = []
+for item in degrees[1:]:
+    degsp.append(item.split())
+
+deglist = []
+for item in degsp:
+    deglist += item
+
+countdict = {x:deglist.count(x)  for x in deglist}
+print countdict
