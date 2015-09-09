f79c2b6f4b41af93c2f240c2f58437fb1754ca4a
David Berger <DBERGER1989@GMAIL.COM>
2015-09-09 00:43:09 -0400

    did p3q1

--- python/advanced_python_dict.py
+++ python/advanced_python_dict.py
@@ -1,1 +1,35 @@
-PLACE YOUR CODE HERE
+##########
+#Part 3 Question 6
+#Creates a dictionary in the appropriate format
+import csv
+import re
+
+fpath = '/Users/David/ds/metis/prework/dsp/python/faculty.csv'
+
+fin = open(fpath, 'rb')
+
+dictfin = csv.DictReader(fin)
+dlist= []
+for item in dictfin:
+    dlist.append(item)
+
+p = re.compile(r'of\s\S+')
+su = []
+for item in dlist:
+    item[' title'] = p.sub('', item[' title'])
+
+na = re.compile(r'\S\w+$')
+for item in dlist:
+    item['name'] = na.search(item['name']).group()
+
+lid = {}
+for item in dlist:
+    nu = {item['name']:[[item[' title'], item[' email']]]}
+    if item['name'] not in lid.keys():
+        lid[item['name']] = [[item[' title'], item[' email']]]
+    else:lid[item['name']].append([item[' title'], item[' email']])
+        
+
+print lid
+
+##################
